e58fa04e5b172ee40f8c3e64735b7dd072648fd8
oscarholt <38869989+oscarholt@users.noreply.github.com>
2022-01-10 12:57:35 +1300

    add input and randomised word cloud

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useRef } from "react"
+
+const getCloud = () =>
+  "oscar testing react nextjs macbook windows"
+    .split(" ")
+    .sort(() => (Math.random() > 0.5 ? 1 : -1))
+
 function App() {
+  const [userInput, setUserInput] = useState("")
+  const cloud = useRef(getCloud())
+
+  
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Typing Test</h1>
+      <p>{cloud.current.toString()}</p>
+      <input
+        type='text'
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
